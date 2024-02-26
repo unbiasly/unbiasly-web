5839030dfef1322a1e969403a50048066f92840d
Satyen19 <satyendra.sant@cvmu.edu.in>
2024-02-26 10:13:09 +0530

    project initialisation

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Box, Typography } from '@mui/material'
+
+export const metadata = {
+  title: 'Next-news',
+  description: 'Next-news',
+}
+
+export default function Home() {
+  return (
+    <Box>
+      <Typography variant='h4'>Next-news</Typography>
+    </Box>
+  )
+}
