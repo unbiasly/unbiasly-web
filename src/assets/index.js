06982c09d6b6bfb45fbed183e41d5f47c52500d4
Satyen-Sen <Satyendra.sant7@gmail.com>
2024-03-19 23:14:59 +0530

    design changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,4 @@
 export { default as illustration } from './illustration.svg'
 export { default as screenshot } from './screenshot.svg'
+export { default as indFlag } from './ind-flag.svg'
+export { default as flag } from './flag.png'
