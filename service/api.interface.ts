33ba1043bc85e115581cded20d78d60d03eb48ac
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2025-02-15 12:56:55 +0530

    API Layer Update

--- service/api.interface.ts
+++ service/api.interface.ts
@@ -1,3 +1,4 @@
+
 export interface BlogPostResponse {
   data: BlogPost[];
   meta: Meta;
@@ -46,9 +47,9 @@ export interface SingleCategoryArticle extends Omit<Article, "category"> {
     category: Category; // Only one category instead of an array
   }
   
-  export interface SingleCategoryNewsArticlesResponse {
-    articles: SingleCategoryArticle[];
-  }
+export interface SingleCategoryNewsArticlesResponse {
+articles: SingleCategoryArticle[];
+}
 
 export interface Article {
   bookmarks: number;
@@ -101,3 +102,69 @@ export interface Source {
 export interface NewsletterSubscribeResponse {
   message: string;
 }
+
+export interface JobAppplicationRequest {
+    full_name: string;
+    contact_information: {
+        email: string;
+        phone: string;
+        address: string;
+    };
+    education: Array<{
+        degree: string;
+        institution: string;
+        dates: string;
+    }>;
+    job_titles: string[];
+    company_names: string[];
+    key_skills: string[];
+    major_achievements?: string[];
+    employment: Array<{
+        position: string;
+        company_name: string;
+        date: string;
+    }>;
+    relevant_certifications: string[];
+    // [key: string]: any;
+}
+
+
+export interface JobApplicationResponse {
+    message: string;
+    }
+
+export interface Department {
+  _id: string;
+  name: string;
+  __v: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface DepartmentResponse {
+  data: Department[];
+}
+
+export interface JobDescription {
+  title: string;
+  company: string;
+  location: string;
+  description: string;
+  experience_level: string;
+  skills: string[];
+}
+
+export interface Job {
+  _id: string;
+  job_id: string;
+  department: string;
+  description: JobDescription;
+  is_expired: boolean;
+  job_name: string;
+  job_type: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+export type JobsByDepartmentResponse = Job[];
--- service/fetchClient.ts
+++ service/fetchClient.ts
@@ -11,12 +11,16 @@ const commonHeaders = {
 
 const fetchClient = {
   get: <T>(url: string) => fetch(url).then<T>(handleResponse),
-  post: <T>(url: string, body: object) =>
-    fetch(url, {
+  post: <T>(url: string, body: object | FormData) => {
+    const headers = body instanceof FormData ? {} : commonHeaders;
+    
+    const requestBody = body instanceof FormData ? body : JSON.stringify(body);
+    return fetch(url, {
       method: "POST",
-      body: JSON.stringify(body),
-      headers: commonHeaders,
-    }).then<T>(handleResponse),
+      body: requestBody,
+      headers: headers as HeadersInit,
+    }).then<T>(handleResponse);
+  },
 };
 
 export default fetchClient;
