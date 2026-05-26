0664a141d653e5e0bff43820dce8ade82e08eae7
unbiasly <155888360+unbiasly@users.noreply.github.com>
2026-05-26 16:05:23 +0530

    Delete app directory

--- app/blog/api/route.ts
+++
@@ -1,8 +0,0 @@
-import AppApi from "@/service/app.api";
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const page = searchParams.get("page") || "1";
-  const data = await AppApi.getBlogPosts(parseInt(page, 10));
-  return Response.json(data);
-}
--- app/live-news/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NewsArticlesRequest } from "@/service/api.interface";
-import AppApi from "@/service/app.api";
-
-export async function POST(request: Request) {
-  const body = (await request.json()) as NewsArticlesRequest;
-  const data = await AppApi.getArticles(body);
-  console.log(data);
-  return Response.json(data);
-}
